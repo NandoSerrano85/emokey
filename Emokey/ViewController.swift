d8c2c1b41f9aad22fc89605f3e3f98107b2cca96
Serrano, Fernando <fernando.serrano@cbsinteractive.com>
2018-09-15 04:52:57 -0400

    downloaded snap kit but could not connect snap kit to project

--- Emokey/ViewController.swift
+++ Emokey/ViewController.swift
@@ -7,11 +7,20 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    lazy var box = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.addSubview(box)
+        box.snp.MakeConstraints{ (make) -> Void in
+            make.width.height.equalTo(50)
+            make.center.equalTo(self.view)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
