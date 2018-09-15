b2738f18a95a1fc46a27138297295b3d8cd96c63
Serrano, Fernando <fernando.serrano@cbsinteractive.com>
2018-09-15 03:25:00 -0400

    Initial Commit

---
+++ Emokey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emokey
+//
+//  Created by Fernando Serrano on 9/15/18.
+//  Copyright © 2018 Teammote. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
